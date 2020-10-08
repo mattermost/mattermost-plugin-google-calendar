a7b23c8d3852e93a2faabf0ba16005f8820f86c2
Edward Dobson <edward.dobson3@ntlworld.com>
2020-10-08 20:38:19 +0100

    [GH-11] Add autocomplete prompts to the command (#23)
    Co-authored-by: Aaron Rothschild <aaron.rothschild@gmail.com>

--- server/command_hooks.go
+++ server/command_hooks.go
@@ -25,7 +25,7 @@ const CommandHelp = `* |/calendar connect| - Connect your Google Calendar with y
 	* |date| should be a date in the format of YYYY-MM-DD or can be "tomorrow" or can be left blank. By default retrieves todays summary breakdown
 * |/calendar create "[title_of_event]" [start_datetime] [end_datetime]| - Create a event with a title and start date-time and end date-time
 	* |title_of_event| can be any title you like for the event. It **MUST** be placed within quotes.
-	* |start_datetime| This is the time the event starts. It should be a date and time in the format of YYYY-MM-DD@HH:MM in 24 hour time format. 
+	* |start_datetime| This is the time the event starts. It should be a date and time in the format of YYYY-MM-DD@HH:MM in 24 hour time format.
 	* |end_datetime| This is the time the event ends. It should be a date and time in the format of YYYY-MM-DD@HH:MM in 24 hour time format.
 `
 
@@ -37,7 +37,33 @@ func getCommand() *model.Command {
 		AutoComplete:     true,
 		AutoCompleteDesc: "Available commands: connect, list, summary, create, help",
 		AutoCompleteHint: "[command]",
+		AutocompleteData: getAutocompleteData(),
 	}
+}
+
+func getAutocompleteData() *model.AutocompleteData {
+	cal := model.NewAutocompleteData("calendar", "[command]", "Available commands: connect, list, summary, create, help")
+
+	connect := model.NewAutocompleteData("connect","", "Connect your Google Calendar with your Mattermost account")
+	cal.AddCommand(connect)
+
+	list := model.NewAutocompleteData("list", "[number_of_events]", "List the upcoming X number of events")
+	list.AddTextArgument("Number of events to list", "[number_of_events]", "^[0-9]+$")
+	cal.AddCommand(list)
+
+	summary := model.NewAutocompleteData("summary","[date]", "Get a breakdown of a particular date")
+	summary.AddTextArgument("The date to view in YYYY-MM-DD format", "[date]", "")
+	cal.AddCommand(summary)
+
+	create := model.NewAutocompleteData("create", "[title_of_event] [start_datetime] [end_datetime]", "Create an event with a title, start date-time and end date-time")
+	create.AddTextArgument("Title for the event you are creating, must be surrounded by quotes", "[title_of_event]", "")
+	create.AddTextArgument("Time the event starts in YYYY-MM-DD@HH:MM format", "[start_datetime]", "")
+	create.AddTextArgument("Time the event finishes in YYYY-MM-DD@HH:MM format", "[end_datetime]", "")
+	cal.AddCommand(create)
+
+	help := model.NewAutocompleteData("help", "", "Display usage")
+	cal.AddCommand(help)
+	return cal
 }
 
 func (p *Plugin) postCommandResponse(args *model.CommandArgs, text string) {
