15374a6553d32f0ea0f844784074ccea4d975be2
Dmitriy Kalekin <herrhorror@gmail.com>
2022-03-22 13:05:33 +0600

    golang-cli-lint error fixed during automake operation: server/plugin.go:15:13: fieldalignment: struct with 72 pointer bytes could be 48 (govet)

--- server/plugin.go
+++ server/plugin.go
@@ -13,16 +13,16 @@ import (
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
 type Plugin struct {
-	plugin.MattermostPlugin
-
-	// configurationLock synchronizes access to the configuration.
-	configurationLock sync.RWMutex
-
 	// configuration is the active plugin configuration. Consult getConfiguration and
 	// setConfiguration for usage.
 	configuration *configuration
 
+	plugin.MattermostPlugin
+
 	botID string
+
+	// configurationLock synchronizes access to the configuration.
+	configurationLock sync.RWMutex
 }
 
 //OnActivate function ensures what bot does when become actived
