dba24e98922df4127490d9d63bc277dd49a43157
kshitij <kshitij.katiyar@brightscout.com>
2024-06-05 16:51:10 +0530

    [MM-73]: Fixed lint errors

--- webapp/src/components/modals/create_event_form.tsx
+++ webapp/src/components/modals/create_event_form.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React, {Key, useState} from 'react';
+import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
 import {Modal} from 'react-bootstrap';
